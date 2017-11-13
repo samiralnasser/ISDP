cd9a922ee89692d39fa46078dfd9cf827a633365
MariaSyed <mariasy@metropolia.fi>
2017-11-13 18:43:10 +0200

    Initilize the react application

---
+++ src/components/List.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class List extends Component {
+    render() {
+        return (
+            <ul>
+                <li>cat</li>
+                <li>dog</li>
+                <li>squirrel</li>
+            </ul>
+        )
+    }
+}
+
+export default List
